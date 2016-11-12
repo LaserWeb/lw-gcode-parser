5f98e7788b6296617f1bb5a6c229b1106fd1b2d6
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-11-13 00:19:13 +0100

    test(): tweaked data type, added data for basic test

--- test/gcodeParser.spec.js
+++ test/gcodeParser.spec.js
@@ -1,12 +1,15 @@
 import test from 'ava'
 // import fs from 'fs' //does not work with babel + brfs
 const fs = require('fs')
+import parse from '../src/index'
 
-test.cb('gcode parser tests: can parse ascii gcode files', t => {
+test.cb('gcode parser tests: can parse gcode files', t => {
+  const gcode = fs.readFileSync('./data/Rotary Axis GCode.gcode','utf8')
+  parse(gcode)
   // this.timeout(5000)
-  fs.createReadStream('./data/test.gcode', { encoding: null, highWaterMark: 512 * 1024 }) // 'binary'
+  /*fs.createReadStream('./data/test.gcode', { encoding: null, highWaterMark: 512 * 1024 }) // 'binary'
     .pipe(makeStlStream())
     .pipe(concatStream(function (parsedgcode) {
       t.end()
-    }))
+    }))*/
 })
