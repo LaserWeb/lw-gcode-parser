6233981c4c0a2d7905b60105a256845e8cbe580b
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-11-13 23:26:33 +0100

    test(): fleshed out basic test a bit

--- test/gcodeParser.spec.js
+++ test/gcodeParser.spec.js
@@ -5,7 +5,14 @@ import parse from '../src/index'
 
 test.cb('gcode parser tests: can parse gcode files', t => {
   const gcode = fs.readFileSync('./data/Rotary Axis GCode.gcode','utf8')
-  parse(gcode)
+  parse(gcode, function(data){
+    console.log('done with parsing',data.metrics)
+    t.deepEqual(data.lineObjects.lines.length, 24)
+    //t.deepEqual(data.positions.length / 3, 864) // we divide by three because each entry is 3 long
+    //t.deepEqual(data.positions[0], -0.025066649541258812)
+    //t.deepEqual(data.positions[data.positions.length - 1], 0.019999999552965164)
+    t.end()
+  })
   // this.timeout(5000)
   /*fs.createReadStream('./data/test.gcode', { encoding: null, highWaterMark: 512 * 1024 }) // 'binary'
     .pipe(makeStlStream())
