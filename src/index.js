0e89145f389835d0b2591ab71ae2c55a2203a200
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-11-13 23:30:04 +0100

    feat(index): added doneCallback, various cleanups & doc additions

--- src/index.js
+++ src/index.js
@@ -1,46 +1,50 @@
 /**
  * @author kaosat-dev / https://github.com/kaosat-dev
  * @author kaosat-dev / https://github.com/kaosat-dev
- *
- * Description: A gcode parser,
+ * for the initial inspiration and example code.
+ * Credit goes to https://github.com/joewalnes/gcode-viewer
+ * GCode descriptions come from:
+ * http://reprap.org/wiki/G-code
+ * http://en.wikipedia.org/wiki/G-code
+ * SprintRun source code
+ * Description: A gcode parser
 **/
-
 import makeHandlers from './gcodeHandlers'
 import parseAsChunks from './parseAsChunks'
-import makeBaseData from './objFromGcode'
+import makeBaseState from './makeBaseState'
 
-export default function parse(data, parameters={}){
-  //const params = Object.assign({}, machineDefaults, parameters)
+export default function parse (data, doneCallback) {
+  // const params = Object.assign({}, machineDefaults, parameters)
+  console.log('Generating GCODE Preview')
 
   const handlers = makeHandlers()
-  let state = makeBaseData().state
+  let state = makeBaseState()
 
-  //FIXME: not sure AT ALL where/when lineObject is supposed to be created
+  // FIXME: not sure AT ALL where/when lineObject is supposed to be created
   state.lineObject = {
     active: false,
-    vertexBuf: new Float32Array(6 * state.bufSize), // Start with bufSize line segments
-    colorBuf: new Float32Array(6 * state.bufSize), // Start with bufSize line segments
+    positions: new Float32Array(6 * state.bufSize), // Start with bufSize line segments
+    colors: new Float32Array(6 * state.bufSize), // Start with bufSize line segments
     nLines: 0
   },
 
-  parseAsChunks(state, handlers, data)
+  parseAsChunks(state, handlers, data, doneCallback)
 }
 
+/*export default function makeGcodeStream (parameters = {}) {
 
-  /*export default function makeGcodeStream (parameters = {}) {
-
-  OLD code structure , for reference !
-  //main entry point !!
-  createObjectFromGCode(gcode, indexMax)
-    => handlers = {
-       gcode(G1 etc): function
-         => drawArc
-         => drawArcFrom2PtsAndCenter
-         => addSegment
-     }
-    =>GCodeParser(handlers)
-      =>parse
-        =>doChunk
-          =>parseLine
-          convertLineGeometryToBufferGeometry
+OLD code structure , for reference !
+//main entry point !!
+createObjectFromGCode(gcode, indexMax)
+  => handlers = {
+     gcode(G1 etc): function
+       => drawArc
+       => drawArcFrom2PtsAndCenter
+       => addSegment
+   }
+  =>GCodeParser(handlers)
+    =>parse
+      =>doChunk
+        =>parseLine
+        convertLineGeometryToBufferGeometry
 }*/
