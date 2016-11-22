83b91732677b2bdb38cf4b8b36a3bed89dc112ca
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-11-22 10:30:36 +0100

    test(): moved test data to external repo, added test for multi material 3dp gcode

--- test/gcodeParser.spec.js
+++ test/gcodeParser.spec.js
@@ -3,20 +3,29 @@ import test from 'ava'
 const fs = require('fs')
 import parse from '../src/index'
 
-test.cb('gcode parser tests: can parse gcode files', t => {
-  const gcode = fs.readFileSync('./data/Rotary Axis GCode.gcode','utf8')
-  parse(gcode, function(data){
-    console.log('done with parsing',data.metrics)
+test.cb('gcode parser: can parse gcode files', t => {
+  const gcode = fs.readFileSync('../node_modules/lw-sample-files/gcode/Rotary Axis GCode.gcode', 'utf8')
+  parse(gcode, function (data) {
+    console.log('done with parsing', data.metrics)
     t.deepEqual(data.linesData.length, 5904)
-    //t.deepEqual(data.positions.length / 3, 864) // we divide by three because each entry is 3 long
-    //t.deepEqual(data.positions[0], -0.025066649541258812)
-    //t.deepEqual(data.positions[data.positions.length - 1], 0.019999999552965164)
+    // t.deepEqual(data.positions.length / 3, 864) // we divide by three because each entry is 3 long
+    // t.deepEqual(data.positions[0], -0.025066649541258812)
+    // t.deepEqual(data.positions[data.positions.length - 1], 0.019999999552965164)
     t.end()
   })
-  // this.timeout(5000)
-  /*fs.createReadStream('./data/test.gcode', { encoding: null, highWaterMark: 512 * 1024 }) // 'binary'
-    .pipe(makeStlStream())
-    .pipe(concatStream(function (parsedgcode) {
-      t.end()
-    }))*/
+/*fs.createReadStream('./data/test.gcode', { encoding: null, highWaterMark: 512 * 1024 }) // 'binary'
+  .pipe(makeStlStream())
+  .pipe(concatStream(function (parsedgcode) {
+    t.end()
+  }))*/
+})
+
+
+test.cb('gcode parser: can parse multi material 3d printer gcode', t => {
+  const gcode = fs.readFileSync('../node_modules/lw-sample-files/gcode/3dp_dualExtrusion_UM3.gcode', 'utf8')
+  parse(gcode, function (data) {
+    console.log('done with parsing', data.metrics)
+    t.deepEqual(data.linesData.length, 759060)
+    t.end()
+  })
 })
